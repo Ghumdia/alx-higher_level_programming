a7513a4e90fd810674ebed151e83a2a9229b642d
Ghumdia <bosass80@gmail.com>
2022-09-21 13:29:25 -0700

    Committed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ if (lastdit > 5):
     print(f"Last digit of {number} is {lastdit} and is greater than 5")
 elif (lastdit == 0):
     print("Last digit of {} is {} and is 0".format(number, lastdit))
-elif (lastdit < 6) and (lastdit != 0):
+elif (lastdit < 6) and (lastdit < 0):
     print(f"Last digit of {number} is {lastdit} and is less than 6 and not 0")
