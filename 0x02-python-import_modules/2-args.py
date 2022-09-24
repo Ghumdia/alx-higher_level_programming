fe69cc6bb5b8e3b8eb3fa69aef2075a738a06bce
Ghumdia <bosass80@gmail.com>
2022-09-24 13:09:56 -0700

    Committed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     argv = len(sys.argv) - 1
     if (argv == 0):
         print("0 argument.")
+    elif argv == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} argument:".format(argv))
+        print("{} arguments:".format(argv))
         for n in range(1, argv + 1):
             print("{}: {}".format(n, sys.argv[n]))
