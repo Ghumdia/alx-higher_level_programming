fd4e3f5d46c264f2a794625363a28526550edb56
Ghumdia <bosass80@gmail.com>
2022-09-26 13:28:21 -0700

    Committed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 and idx > (len(my_list) - 1):
         return (my_list)
-    else:
-        tmp = [x for x in my_list]
-        tmp[idx] = element
-        return (tmp)
+
+    tmp = [x for x in my_list]
+    tmp[idx] = element
+    return (tmp)
