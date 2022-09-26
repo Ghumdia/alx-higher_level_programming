5507899ee5d2644a7dcc9b6dc210c4048720d3d6
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-09-26 10:12:31 -1100

    Committed

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    mul = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+          mul.append(True)
+         else:
+          mul.append(False)
+            
