f29f2b1181719b5dae28d4220f20cb210535d187
Ghumdia <bosass80@gmail.com>
2022-09-26 14:20:55 -0700

    Committed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@ def divisible_by_2(my_list=[]):
     mul = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-          mul.append(True)
+            mul.append(True)
          else:
-          mul.append(False)
+             mul.append(False)
     return (mul)
