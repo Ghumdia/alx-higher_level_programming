4032324af69f809769491bc064e6f7c238c2d081
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-10-11 07:14:36 -1100

    Committed

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+def list_division(my_list_1, my_list_2, list_length):
+    newlist = []
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+            newlist.append(div)
+        except TypeError:
+            print("wrong type")
+            div = 0
+            newlist.append(div)
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+            newlist.append(div)
+        except IndexError:
+            print("out of range")
+            div = 0
+            newlist.append(div)
+    return newlist
