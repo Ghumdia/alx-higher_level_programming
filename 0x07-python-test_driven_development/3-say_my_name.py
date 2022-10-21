acd0863781e4e24462982dd5e935f764195b5f5b
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2022-10-21 11:38:39 -1100

    Committed

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Define say my name"""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints users name"""
-
-    if not isinstanc(first_name, str):
+    """Print a name."""
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
