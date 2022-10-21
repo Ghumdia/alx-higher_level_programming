d28fad042d32605c99d31ffd1d0e66186b3e3df6
Ghumdia <bosass80@gmail.com>
2022-10-21 15:03:53 -0700

    Committed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" Defines an integer"""
+
+
+
 def add_integer(a, b=98):
+    """ Returns the addition of a and b"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
