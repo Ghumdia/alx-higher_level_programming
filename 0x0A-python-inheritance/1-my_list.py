2452b17d7ad6d1b9c49e32acbedb73182b393126
Ghumdia <bosass80@gmail.com>
2022-10-25 08:54:33 -0700

    Committed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,7 @@
 
 
 class MyList(list):
-    """Implement sorted printing"""
-
+    """Implement sorted printing for built-in list class"""
     def print_sorted(self):
         """Returns a sorted list"""
-
         return(sorted(self))
