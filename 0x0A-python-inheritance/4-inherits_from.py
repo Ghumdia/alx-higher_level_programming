a4041ba6d0ff11d900b03d875d150b7779bea02d
Ghumdia <bosass80@gmail.com>
2022-10-25 12:45:12 -0700

    Committed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines an inherited class"""
 
+
 def inherits_from(obj, a_class):
     """Returns true or flase"""
-
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
