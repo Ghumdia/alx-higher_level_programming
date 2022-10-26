eb762c94dbe684fe704abbe57f62bee3c2b78ede
Ghumdia <bosass80@gmail.com>
2022-10-26 12:51:27 -0700

    Committed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """Reads line and prints every line"""
     with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
