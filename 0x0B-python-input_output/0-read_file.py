e4cfe4d72ee462f4abc375838f9a352f6a2d8a6b
Ghumdia <bosass80@gmail.com>
2022-10-26 12:49:44 -0700

    Committed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,7 @@
 """Reads a file in stout"""
 
 
-def read_file(filesname=""):
+def read_file(filename=""):
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
