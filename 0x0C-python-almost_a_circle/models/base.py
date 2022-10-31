8c1439cdb2cc3be9a43990583b2c3e63df0066dd
Ghumdia <bosass50@gmail.com>
2022-10-31 12:29:28 -0700

    Committed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
 """First base"""
 
 
 class Base:
     """A base class"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialization file"""
-        if id == None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
