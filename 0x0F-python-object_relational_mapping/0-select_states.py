6c4059a8761b284b8b834849dbe26b4b2fc99b73
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-01-27 14:50:02 +0100

    Committed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 # Lists all states from the database hbtn_0e_0_usa.
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
 import sys
+#Imports both sys and MySQLdb module
 import MySQLdb
 #Imports both sys and MySQLdb module
 
