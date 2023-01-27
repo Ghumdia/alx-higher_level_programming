3f8f2de573103ec8540309dc04c41c6625441abc
Ghumdia <106809317+Ghumdia@users.noreply.github.com>
2023-01-27 14:52:38 +0100

    Committed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-# Lists all states from the database hbtn_0e_0_usa.
-
 import sys
-#Imports both sys and MySQLdb module
 import MySQLdb
-#Imports both sys and MySQLdb module
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 c = db.cursor()
