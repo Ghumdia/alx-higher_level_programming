7015c194c90d2ff282ff6aeca41bfb2d1282634e
Ghumdia <bosass80@gmail.com>
2023-01-27 06:23:56 -0800

    Committed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Takes an argument and searchs the entire 
+Takes an argument and searchs the entire
 database for that argument taken in
 """
 
@@ -11,7 +11,9 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM `states` \
+            WHERE name LIKE BINARY '{}' ORDER \
+            BY id ASC".format(sys.argv[4]))
     for state in cur.fetchall():
         print(state)
 
