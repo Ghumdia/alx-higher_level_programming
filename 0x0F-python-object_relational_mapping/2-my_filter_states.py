f5154be89b8be791f8b3d1e93b0d150ef1df5e9f
Ghumdia <bosass80@gmail.com>
2023-01-27 06:22:10 -0800

    Committed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Takes an argument and searchs the entire 
+database for that argument taken in
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    for state in cur.fetchall():
+        print(state)
+
+    cur.close()
+    db.close()
