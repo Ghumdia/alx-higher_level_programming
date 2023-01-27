b9bb8e8dabfdbfa10f2873fa8d26095f09576171
Ghumdia <bosass80@gmail.com>
2023-01-27 07:08:24 -0800

    Committed

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+this moducle contains a base which a class onherits
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+
+Base = declarative_base()
+
+class State(Base):
+
+    """
+    The states class inherits the base class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
