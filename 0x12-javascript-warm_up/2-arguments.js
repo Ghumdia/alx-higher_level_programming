e6ee3f1ecda1783f206246020116c7412ecfb75e
John Billy <bosass80@gmail.com>
2023-02-21 20:22:11 +0000

    Committed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 // Checks if there were arguments given on command line
-let ag = process.argv ;
+const ag = process.argv;
 let i = 0;
-while ( ag[i] ){
-	i++;
+while (ag[i]) {
+  i++;
 }
 i -= 2;
-if ( i === 0) {
-	console.log('No argument');
-}
-else if ( i === 1 ) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+if (i === 0) {
+  console.log('No argument');
+} else if (i === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
