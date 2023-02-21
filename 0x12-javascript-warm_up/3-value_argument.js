9591fef461af72493c20d63ff84a5730783480bb
John Billy <bosass80@gmail.com>
2023-02-21 20:37:22 +0000

    Committed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Prints only the first argument
+const ag = process.argv;
+let i = 0;
+while (ag[i]) {
+  i++;
+}
+if (i > 3) {
+  const tx = ag[2];
+  console.log(tx);
+}
+else {
+  console.log('No argument');
+}
