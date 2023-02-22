115aab2849115dbb05325c86d7904dfdb7ebfa80
John Billy <bosass80@gmail.com>
2023-02-22 12:22:03 +0000

    Committed

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints 3 lines: but by using an array of string and a loop
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (arr[i]) {
+  console.log(arr[i]);
+  i++;
+}
