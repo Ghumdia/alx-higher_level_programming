0d3410c5ba0264a7f015f6439abe65eeafc17439
John Billy <bosass80@gmail.com>
2023-03-16 23:53:17 +0000

    Committed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,10 +1,22 @@
-const request = require('request');
-const ag = process.argv;
+#!/usr/bin/node
+//In order
 
-request('https://swapi-api.alx-tools.com/api/films/' + ag[2], (error, response, body) => {
-  if (error) {
-    console.error(error);
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-  const data = JSON.parse(body);
-  console.log(data.title);
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
